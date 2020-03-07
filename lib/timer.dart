877d42b492e73a6174c2b6855648ec1189780772
Quentin <quentin.beaussart@gmail.com>
2020-03-07 15:03:49 +0100

    Fix timer button

--- lib/timer.dart
+++ lib/timer.dart
@@ -26,6 +26,8 @@ class _TimerPageState extends State<TimerPage> with TickerProviderStateMixin {
   static AudioCache player = new AudioCache();
   static const alarmAudioPath = "sounds/end_sound.mp3";
 
+  bool playing = false;
+
   String get timerString {
     Duration duration = controller.duration * controller.value;
     return '${duration.inMinutes}:${(duration.inSeconds % 60).toString().padLeft(2, '0')}';
@@ -83,168 +85,172 @@ class _TimerPageState extends State<TimerPage> with TickerProviderStateMixin {
         ),
         child :
         AnimatedBuilder(
-            animation: controller
-              ..addStatusListener((status) {
-                //on checke si le timer est terminé grâce à son animation
-              if (status == AnimationStatus.dismissed) {
-                player.play(alarmAudioPath);
-              }
-            }),
-            builder: (context, child) {
-              return Padding(
-                padding: EdgeInsets.fromLTRB(45, 90, 45, 40),
-                child:
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Container(
-                      margin : EdgeInsets.all(10),
-                      decoration: ShapeDecoration(
-                        color: Colors.blue,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(60.0)),
-                        ),
+          animation: controller
+            ..addStatusListener((status) {
+              //on checke si le timer est terminé grâce à son animation
+            if (status == AnimationStatus.dismissed) {
+              player.play(alarmAudioPath);
+            }
+          }),
+          builder: (context, child) {
+            return Padding(
+              padding: EdgeInsets.fromLTRB(45, 90, 45, 40),
+              child:
+              Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Container(
+                    margin : EdgeInsets.all(10),
+                    decoration: ShapeDecoration(
+                      color: Colors.blue,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(60.0)),
                       ),
+                    ),
+                    child:
+                    DropdownButtonHideUnderline(
                       child:
-                      DropdownButtonHideUnderline(
-                        child:
-                        DropdownButton<String>(
-                            hint:
-                            Container(
-                              margin : EdgeInsets.all(15),
+                      DropdownButton<String>(
+                          hint:
+                          Container(
+                            margin : EdgeInsets.all(15),
+                            child:
+                            Text('Sélectionnez une durée',
+                              //style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          items: _duree.map((String value) {
+                            return new DropdownMenuItem<String>(
+                              value: value,
                               child:
-                              Text('Sélectionnez une durée',
-                                //style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+                              Container(
+                                margin : EdgeInsets.all(15),
+                                child:
+                                new Text(value),
+                              ),
+                            );
+                          }).toList(),
+                          onChanged: (String value) {
+                            setState(() {
+                              _dureeSelected = value;
+                              switch(value){
+                                case '5 minutes' : {
+                                  _timerSelected = 5*1; //TODO change the value to 60 after tests
+                                }
+                                break;
+                                case '10 minutes' : {
+                                  _timerSelected = 10*60;
+                                }
+                                break;
+                                case '15 minutes' : {
+                                  _timerSelected = 15*60;
+                                }
+                                break;
+                                case '20 minutes' : {
+                                  _timerSelected = 20*60;
+                                }
+                                break;
+                                case '25 minutes' : {
+                                  _timerSelected = 25*60;
+                                }
+                                break;
+                                case '30 minutes' : {
+                                  _timerSelected = 30*60;
+                                }
+                                break;
+                                case '35 minutes' : {
+                                  _timerSelected = 35*60;
+                                }
+                                break;
+                                case '40 minutes' : {
+                                  _timerSelected = 40*60;
+                                }
+                                break;
+                              }
+                              majTimer();
+                            });
+                          },
+                          value: _dureeSelected
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Align(
+                      alignment: FractionalOffset.center,
+                      child: AspectRatio(
+                        aspectRatio: 1.0,
+                        child: Stack(
+                          children: <Widget>[
+                            Positioned.fill(
+                              child:
+                              AnimatedBuilder(
+                                animation: controller,
+                                builder:
+                                    (BuildContext context, Widget child) {
+                                  return CustomPaint(
+                                      painter: CustomTimerPainter(
+                                        animation: controller,
+                                        backgroundColor: Colors.white,
+                                        color: themeData.indicatorColor,
+                                      )
+                                  );
+                                },
                               ),
                             ),
-                            items: _duree.map((String value) {
-                              return new DropdownMenuItem<String>(
-                                value: value,
-                                child:
-                                Container(
-                                  margin : EdgeInsets.all(15),
-                                  child:
-                                  new Text(value),
-                                ),
-                              );
-                            }).toList(),
-                            onChanged: (String value) {
+                            Align(
+                              alignment: FractionalOffset.center,
+                              //timer affiché
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: <Widget>[
+                                  AnimatedBuilder(
+                                    animation: controller,
+                                    builder: (BuildContext context,
+                                        Widget child) {
+                                      return Text(
+                                        timerString,
+                                        style: TextStyle(
+                                            fontSize: 112.0,
+                                            color: Colors.white),
+                                      );
+                                    },
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  AnimatedBuilder(
+                    animation: controller,
+                    builder: (context, child) {
+                      return FloatingActionButton.extended(
+                          onPressed: () {
+                            if (controller.isAnimating){
+                              controller.stop();
                               setState(() {
-                                _dureeSelected = value;
-                                switch(value){
-                                  case '5 minutes' : {
-                                    _timerSelected = 5*1; //TODO change the value to 60 after tests
-                                  }
-                                  break;
-                                  case '10 minutes' : {
-                                    _timerSelected = 10*60;
-                                  }
-                                  break;
-                                  case '15 minutes' : {
-                                    _timerSelected = 15*60;
-                                  }
-                                  break;
-                                  case '20 minutes' : {
-                                    _timerSelected = 20*60;
-                                  }
-                                  break;
-                                  case '25 minutes' : {
-                                    _timerSelected = 25*60;
-                                  }
-                                  break;
-                                  case '30 minutes' : {
-                                    _timerSelected = 30*60;
-                                  }
-                                  break;
-                                  case '35 minutes' : {
-                                    _timerSelected = 35*60;
-                                  }
-                                  break;
-                                  case '40 minutes' : {
-                                    _timerSelected = 40*60;
-                                  }
-                                  break;
-                                }
-                                majTimer();
+                                playing = false;
                               });
-                            },
-                            value: _dureeSelected
-                        ),
-                      ),
-                    ),
-                    Expanded(
-                      child: Align(
-                        alignment: FractionalOffset.center,
-                        child: AspectRatio(
-                          aspectRatio: 1.0,
-                          child: Stack(
-                            children: <Widget>[
-                              Positioned.fill(
-                                child:
-                                AnimatedBuilder(
-                                  animation: controller,
-                                  builder:
-                                      (BuildContext context, Widget child) {
-                                    return CustomPaint(
-                                        painter: CustomTimerPainter(
-                                          animation: controller,
-                                          backgroundColor: Colors.white,
-                                          color: themeData.indicatorColor,
-                                        )
-                                    );
-                                  },
-                                ),
-                              ),
-                              Align(
-                                alignment: FractionalOffset.center,
-                                //timer affiché
-                                child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                                  crossAxisAlignment: CrossAxisAlignment.center,
-                                  children: <Widget>[
-                                    AnimatedBuilder(
-                                        animation: controller,
-                                        builder: (BuildContext context,
-                                            Widget child) {
-                                          return Text(
-                                            timerString,
-                                            style: TextStyle(
-                                                fontSize: 112.0,
-                                                color: Colors.white),
-                                          );
-                                        }
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
-                    ),
-                    AnimatedBuilder(
-                        animation: controller,
-                        builder: (context, child) {
-                          return FloatingActionButton.extended(
-                              onPressed: () {
-                                if (controller.isAnimating){
-                                  controller.stop();
-                                }
-                                else {
-                                  controller.reverse();
-                                }
-                              },
-                              icon: Icon(
-                                  controller.isAnimating ? Icons.pause : Icons.play_arrow),
-                              label: Text(
-                                  controller.isAnimating ? "Pause" : "Démarrer")
-                          );
-                        }
-                    ),
-                  ],
-                ),
-              );
-            }
+                            }
+                            else {
+                              controller.reverse();
+                              setState(() {
+                                playing = true;
+                              });
+                            }
+                          },
+                          icon: Icon(playing ? Icons.pause : Icons.play_arrow),
+                          label: Text(playing ? "Pause" : "Démarrer"),
+                      );
+                    },
+                  ),
+                ],
+              ),
+            );
+          },
         ),
       ),
       bottomNavigationBar: BottomNavigationBar(
