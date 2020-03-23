c60cf8d8b2e0068c8c1b950434231f8772e5aba5
Quentin <quentin.beaussart@gmail.com>
2020-03-23 19:47:56 +0100

    Firebase setup OK

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_analytics/observer.dart';
 import 'package:flutter/material.dart';
 import 'menu.dart';
 
@@ -5,15 +7,23 @@ void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+  static FirebaseAnalytics analytics = FirebaseAnalytics();
+  static FirebaseAnalyticsObserver observer = FirebaseAnalyticsObserver(analytics: analytics);
   @override
   Widget build(BuildContext context) {
+
     return new MaterialApp(
       title: 'Meditate',
       theme: new ThemeData(
         primarySwatch: Colors.lightBlue,
         fontFamily: 'Nunito',
       ),
-      home: new MenuPage(title: 'Medidate - Menu principal'),
+      navigatorObservers: <NavigatorObserver>[observer],
+      home: new MenuPage(
+          title: 'Medidate - Menu principal',
+          analytics: analytics,
+          observer: observer,
+      ),
     );
   }
 }
